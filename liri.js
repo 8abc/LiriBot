fc6728b0a614bf6729240774a18630d738429f8d
Abc <anadelaine.catalla@gmail.com>
2019-05-14 21:14:41 -0700

    created js, json, txt

---
+++ liri.js
@@ -0,0 +1,18 @@
+// to read and set any environment variables with the dotenv package:
+js;
+require("dotenv").config();
+
+// to import the `keys.js` file and store it in a variable.
+js;
+var keys = require("./keys.js");
+
+js;
+var spotify = new Spotify(keys.spotify);
+
+// concert-this
+
+//  spotify-this-song
+
+//    movie-this
+
+//    do-what-it-says
